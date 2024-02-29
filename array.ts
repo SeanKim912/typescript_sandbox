332055c5b4f60b7096c5dfa254c7ae4ac5f9ec18
Sean <seankim912@yahoo.com>
2024-02-29 00:23:46 -0600

    arrays and tuples in typescript

---
+++ array.ts
@@ -0,0 +1,63 @@
+// Array Type Annotations: Two main syntaxes for array type annotations
+let names: string[] = ['John', 'Paul', 'George', 'Ringo'];
+let names2: Array<string> = ['John', 'Paul', 'George', 'Ringo'];
+// Both the above will throw type errors if assigned with the wrong type or an element of the wrong type is pushed
+
+// Multi-dimensional Arrays
+let arr: string[][] = [['str1', 'str2'], ['more', 'strings']];
+
+// Empty arrays are compatible with any other array type
+let names3: string[] = []; // No type errors.
+
+/*
+    Typescript Tuples:
+    With Typescript, you can also define arrays with a fixed sequence fo types.
+    Tuple types specify both the lengths and orders of compatible tuples, and will cause errors if those are not followed.
+*/
+let ourTuple: [string, number, string, boolean] = ['Is', 7 , 'our favorite number?' , false];
+
+/*
+    Tuples act just like arrays as far as Javascript is concerned, but in Typescript they are not compatible. You CAN'T
+    assign an array to a tuple variable, even whwen elements are of the correct type.
+*/
+let tup: [string, string] = ['hi', 'bye'];
+let arr2: string[] = ['there','there'];
+tup = ['there', 'there']; // No Errors.
+// tup = arr2; // Type Error! An array cannot be assigned to a tuple.
+
+/*
+    Array Type Inference:
+    Typescript can infer the type of an array from the type of the elements in the array.
+*/
+
+let examAnswers = [true, false, false];
+/*
+    The type could be boolean[] or [boolean, boolean, boolean]. Typescript will infer the first because it is less restrictive
+    and lets you expand the array.
+*/
+examAnswers[3] = true; // No errors since less restrictive type is boolean[] and not [boolean, boolean, boolean]
+
+let tup2: [number, number, number] = [1,2,3];
+let concatResult = tup2.concat([4,5,6]); // concatResult has the value [1,2,3,4,5,6].
+// In the code above, TypeScript infers the variable concatResult as an array of numbers, not a tuple.
+
+// Rest Parameters:
+function smush(firstString, ...otherStrings: string[]) {
+    let output = firstString;
+        for(let i = 0; i < otherStrings.length; i++){
+        output = output.concat(otherStrings[i]);
+    }
+    return output;
+}
+
+// Array of tuples typing
+let danceMoves: [string, number, boolean][] = [
+    ['chicken beak', 4, false],
+    ['wing flap', 4, false],
+    ['tail feather shake', 4, false],
+    ['clap', 4, false],
+    ['chicken beak', 4, true],
+    ['wing flap', 4, true],
+    ['tail feather shake', 4, true],
+    ['clap', 4, true]
+];
--- test.ts
+++ test.ts
@@ -95,3 +95,13 @@ function logGreeting(name:string): void{
 function getSum(x: number, y: number): number { // Try hovering over getSum to see the documentation comment.
     return x + y;
 }
+
+// Spread Syntax:
+function gpsNavigate(startLatitudeDegrees:number, startLatitudeMinutes:number, startNorthOrSouth:string, startLongitudeDegrees: number, startLongitudeMinutes: number, startEastOrWest:string, endLatitudeDegrees:number, endLatitudeMinutes:number , endNorthOrSouth:string, endLongitudeDegrees: number, endLongitudeMinutes: number,  endEastOrWest:string) {
+    /* navigation subroutine here */
+}
+
+let codecademyCoordinates: [number, number, string, number, number, string] = [40, 43.2, 'N', 73, 59.8, 'W'];
+let bermudaTCoordinates: [number, number, string, number, number, string] = [25, 0 , 'N' , 71, 0, 'W'];
+
+gpsNavigate(...codecademyCoordinates, ...bermudaTCoordinates);
